85c5a7996e90fd6be9e0dcf9407c53e5bc6f7f09
Rodrigo França <rfrancacostagmail.com>
2018-04-18 23:16:45 -0300

    Alteração no método gravar para receber um RedirectAttributes edevolver um ModelAndView. Adicionado a mensagem de sucesso no lista.jsp.

--- prjSpringMVCI/src/main/java/br/com/franca/controllers/ProdutosController.java
+++ prjSpringMVCI/src/main/java/br/com/franca/controllers/ProdutosController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.franca.daos.ProdutoDAO;
 import br.com.franca.models.Produto;
@@ -27,12 +28,15 @@ public class ProdutosController {
 		return modelAndView;
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
-	public String gravar(Produto produto) {
-		System.out.println(produto);
-		produtoDao.gravar(produto);
-		return "/produtos/ok";
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes){
+	    System.out.println(produto);
+	    produtoDao.gravar(produto);
+	    redirectAttributes.addFlashAttribute("sucesso","Produto cadastrado com sucesso!");
+	    return new ModelAndView("redirect:produtos");
 	}
+
+
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView listar() {
--- prjSpringMVCI/src/main/webapp/WEB-INF/views/produtos/lista.jsp
+++ prjSpringMVCI/src/main/webapp/WEB-INF/views/produtos/lista.jsp
@@ -1,9 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-
-<!-- Import da taglib -->
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,7 +10,7 @@
 </head>
 <body>
 	<h1>Lista de Produtos</h1>
-	
+	<p>${sucesso}</p>
 	<table>
 		<tr>
 			<td>Título</td>
@@ -29,7 +26,5 @@
 			</tr>
 		</c:forEach>
 	</table>
-
 </body>
-
 </html>
