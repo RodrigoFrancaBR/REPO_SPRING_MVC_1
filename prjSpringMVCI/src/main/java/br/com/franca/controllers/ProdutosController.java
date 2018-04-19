52470fb55f26b4850eb491445c958221bca1f661
Rodrigo França <rfrancacostagmail.com>
2018-04-18 23:49:55 -0300

    Criaçã da classe ProdutoValidation.

--- prjSpringMVCI/src/main/java/br/com/franca/controllers/ProdutosController.java
+++ prjSpringMVCI/src/main/java/br/com/franca/controllers/ProdutosController.java
@@ -2,8 +2,13 @@ package br.com.franca.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.franca.daos.ProdutoDAO;
 import br.com.franca.models.Produto;
 import br.com.franca.models.TipoPreco;
+import br.com.franca.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
@@ -28,15 +34,15 @@ public class ProdutosController {
 		return modelAndView;
 	}
 
-	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes){
-	    System.out.println(produto);
-	    produtoDao.gravar(produto);
-	    redirectAttributes.addFlashAttribute("sucesso","Produto cadastrado com sucesso!");
-	    return new ModelAndView("redirect:produtos");
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
+		if (result.hasErrors()) {
+			return form();
+		}
+		produtoDao.gravar(produto);
+		redirectAttributes.addFlashAttribute("message", "Produto cadastrado com sucesso");
+		return new ModelAndView("redirect:produtos");
 	}
-
-
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView listar() {
@@ -44,6 +50,11 @@ public class ProdutosController {
 		ModelAndView modelAndView = new ModelAndView("/produtos/lista");
 		modelAndView.addObject("produtos", produtos);
 		return modelAndView;
+	}
+
+	@InitBinder
+	public void InitBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
 	}
 
 }
